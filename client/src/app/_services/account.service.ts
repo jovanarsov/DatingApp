d5b3142d3ed0f3df24398b2b4b16abac04f3eb8c
Jovan Arsov <jovana@ever-near.com>
2022-04-10 17:08:24 +0200

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  
+  //ReplaySubject e tip na Observable, Buffer Object gi smestuva vrednostite vo nego
+  //sekoj pat koga nekoj kje se subscribe, kje gi prikaze poslednite vrednosti vo nego
+  //ili kolku so ima vo nego, kolku so sakame da pokazime (1)
+  private currentUserSource = new ReplaySubject<User>(1); 
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any)
+  {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if(user)
+        {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) =>{
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        //return user; moze tuka da go vratam
+        //i kje go prikazam noviot registriran vo register() vo reg.component.ts
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
