d4eb832b9a0eabea105f8e069bccc94a2be040a6
Jovan Arsov <jovana@ever-near.com>
2022-04-28 15:37:47 +0200

    End of Section 17 (SignalR)

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,7 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1); 
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
   login(model: any)
   {
@@ -27,6 +28,7 @@ export class AccountService {
         if(user)
         {
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
     )
@@ -37,6 +39,7 @@ export class AccountService {
       map((user: User) =>{
         if(user){
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
         //return user; moze tuka da go vratam
         //i kje go prikazam noviot registriran vo register() vo reg.component.ts
@@ -55,6 +58,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presence.stopHubConnection();
   }
 
   getDecodedToken(token){
